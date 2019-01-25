0a131f50818d1833d27195ec21ede44f2158cefe
geass94 <okoridze@yahoo.com>
2019-01-25 19:18:16 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
+  constructor(){
+    window['$'] = window['jQuery'] = $;
+  }
   title = 'kedi-app';
 }
