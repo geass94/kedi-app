6c698b76dec455cb489400c0d93b77da9af0aa9f
geass94 <okoridze@yahoo.com>
2019-02-17 22:34:29 +0400

    mari: add to cart and wishlist

--- src/app/components/public/shop/shop.component.ts
+++ src/app/components/public/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Product} from "../../../models/product";
 import {ProductService} from "../../../services/product.service";
 import {deserialize} from "serializer.ts/Serializer";
@@ -6,14 +6,15 @@ import {Menu} from "../../../models/menu";
 import {MenuService} from "../../../services/menu.service";
 import {User} from "../../../models/user";
 import {UserService} from "../../../services/user.service";
-
+import {CartService} from "../../../services/cart.service";
+declare var jQuery: any;
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent implements OnInit {
+export class ShopComponent implements OnInit, AfterViewInit {
   loggedUser: User;
   products: Product[];
   sideBar: Menu = new Menu;
@@ -21,7 +22,8 @@ export class ShopComponent implements OnInit {
   constructor(
     private productService: ProductService,
     private menuService: MenuService,
-    private userService: UserService
+    private userService: UserService,
+    private cartService: CartService
   ) {
     this.loggedUser = this.userService.loadProfile();
   }
@@ -40,5 +42,26 @@ export class ShopComponent implements OnInit {
     }).unsubscribe();
   }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product.id).subscribe();
+  }
+
+  priceSliderOnChange(v: any) {
+    console.log(v);
+  }
+
+  ngAfterViewInit() {
+    jQuery( "#slider-range" ).slider({
+      range: true,
+      min: 100,
+      max: 750,
+      values: [ 100, 700 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "" + ui.values[ 0 ] + " -- " + ui.values[ 1 ] );
+      }
+    });
+    jQuery( "#amount" ).val( "" + jQuery( "#slider-range" ).slider( "values", 0 ) +
+      " -- " + jQuery( "#slider-range" ).slider( "values", 1 ) );
+  }
 
 }
