09ef899a8497888bd9a6318853637cfecde93c52
geass94 <okoridze@yahoo.com>
2019-02-18 00:01:47 +0400

    product sorting

--- src/app/components/public/shop/shop.component.ts
+++ src/app/components/public/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import {Product} from "../../../models/product";
 import {ProductService} from "../../../services/product.service";
 import {deserialize} from "serializer.ts/Serializer";
@@ -18,6 +18,9 @@ export class ShopComponent implements OnInit, AfterViewInit {
   loggedUser: User;
   products: Product[];
   sideBar: Menu = new Menu;
+  maxPrice = 0;
+  minPrice = 0;
+  private allProducts: Product[];
 
   constructor(
     private productService: ProductService,
@@ -33,9 +36,26 @@ export class ShopComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.productService.getProducts().subscribe(res => {
+    this.productService.getProducts().subscribe(
+      (res) => {
       this.products = deserialize<Product[]>(Product, res);
-    });
+      },
+      (err) => {
+
+      },
+      () => {
+        this.maxPrice = Math.max.apply(null, this.products.map(function(item) {
+          return item.price;
+        }));
+
+        this.minPrice = Math.min.apply(null, this.products.map(function(item) {
+          return item.price;
+        }));
+
+        this.allProducts = this.products;
+
+        this.initPriceSlider();
+      });
 
     this.menuService.getSideBar().subscribe(res => {
       this.sideBar = deserialize<Menu>(Menu, res);
@@ -46,22 +66,39 @@ export class ShopComponent implements OnInit, AfterViewInit {
     this.cartService.addToCart(product.id).subscribe();
   }
 
-  priceSliderOnChange(v: any) {
-    console.log(v);
-  }
 
-  ngAfterViewInit() {
+  private initPriceSlider() {
     jQuery( "#slider-range" ).slider({
       range: true,
-      min: 100,
-      max: 750,
-      values: [ 100, 700 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "" + ui.values[ 0 ] + " -- " + ui.values[ 1 ] );
+      min: this.minPrice,
+      max: this.maxPrice,
+      values: [ this.minPrice, this.maxPrice ],
+      slide: ( event, ui ) => {
+        jQuery( "#amount" ).val( "" + ui.values[ 0 ] + " -- " + ui.values[ 1 ] );
+        this.filterProductsByPriceRange(ui.values[ 0 ], ui.values[ 1 ]);
       }
     });
     jQuery( "#amount" ).val( "" + jQuery( "#slider-range" ).slider( "values", 0 ) +
       " -- " + jQuery( "#slider-range" ).slider( "values", 1 ) );
+  }
+
+  private filterProductsByPriceRange(min, max) {
+    this.products = this.allProducts.filter(e => e.price >= min && e.price <= max);
+  }
+
+  priceSliderOnChange(v: any) {
+    let arr = v.split(" -- ");
+    this.minPrice = parseFloat(arr[0]);
+    this.maxPrice = parseFloat(arr[1]);
+    jQuery( "#slider-range" ).slider({
+      min: this.minPrice,
+      max: this.maxPrice,
+    });
+    this.filterProductsByPriceRange(this.minPrice, this.maxPrice);
+  }
+
+  ngAfterViewInit() {
+
   }
 
 }
